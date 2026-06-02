c6a979a4527b1a8742f1a5b872ceacf71754a579
whimsiemi <mochanitrate@gmail.com>
2026-06-02 23:34:19 +0100

    Revert "grammar should be fixed"
    This reverts commit 801c99a31a61f957e4802016632ff0fdf623512e.

--- commands/utility/secondary.js
+++ commands/utility/secondary.js
@@ -100,25 +100,15 @@ module.exports = {
           dataset_name = "chosen";
           break;
       }
-      if (char_secs.length > 1) {
-        const secsHeaders = new TextDisplayBuilder().setContent(
-          "**Here are the " +
-            interaction.options.getInteger("size") +
-            " best secondaries for " +
-            player_main +
-            ", based on the " +
-            dataset_name +
-            " dataset:**",
-        );
-      } else {
-        const secsHeaders = new TextDisplayBuilder().setContent(
-          "**Here is the best secondary for " +
-            player_main +
-            ", based on the " +
-            dataset_name +
-            " dataset:**",
-        );
-      }
+      const secsHeaders = new TextDisplayBuilder().setContent(
+        "**Here are the " +
+          interaction.options.getInteger("size") +
+          " best secondaries for " +
+          player_main +
+          ", based on the " +
+          dataset_name +
+          " dataset:**",
+      );
 
       const separator = new SeparatorBuilder()
         .setDivider(false) // No line displayed
